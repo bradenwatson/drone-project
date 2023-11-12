5cd83515befe53272c6988fb81fbb9647cab9af2
Braden Watson <brwatson10@gmail.com>
2023-11-12 11:13:14 +0800

    Add 'Users' class in 'models.py'

--- droneproject/drones/models.py
+++ droneproject/drones/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Users(AbstractUser):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=200, unique=True)
+    email = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=256)
+
+    def __str__(self):
+        return str(self.username)
