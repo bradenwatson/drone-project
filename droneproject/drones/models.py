2384e186c7a2d46f18ee06b2d5afd70a876c6880
Braden Watson <brwatson10@gmail.com>
2023-11-12 11:14:08 +0800

    Add 'AP' class in 'models.py'

--- droneproject/drones/models.py
+++ droneproject/drones/models.py
@@ -10,3 +10,16 @@ class Users(AbstractUser):
 
     def __str__(self):
         return str(self.username)
+
+
+class AP(models.Model):
+    ap_id = models.AutoField(primary_key=True)
+    ssid = models.CharField(max_length=32, unique=True)
+    password = models.CharField(max_length=256)
+    auth_method = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(Users, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return str(self.ssid)
