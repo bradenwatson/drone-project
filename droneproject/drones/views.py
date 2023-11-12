4cbb84171e9846c883d3d6f38fb9333313a8ec88
Braden Watson <brwatson10@gmail.com>
2023-11-12 16:07:51 +0800

    Import all the modules in views.py

--- droneproject/drones/views.py
+++ droneproject/drones/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import SetPasswordForm
+from .forms import UserRegistrationForm, UpdateUserForm, APForm, SwarmsForm, DronesForm
+from .models import AP, Swarms, Drones
 
 # Create your views here.
