495e70e3600fc492ac9b9f241bf387cd7586f6fd
Braden Watson <brwatson10@gmail.com>
2023-11-13 11:43:31 +0800

    Add drone_commands.py & populate with commands from simple.py

---
+++ droneproject/drone_commands.py
@@ -0,0 +1,12 @@
+from DJITelloPy.djitellopy import Tello
+
+tello = Tello("127.0.0.1")
+
+tello.connect()
+tello.takeoff()
+
+tello.move_left(50)
+tello.rotate_clockwise(90)
+tello.move_forward(50)
+
+tello.land()
